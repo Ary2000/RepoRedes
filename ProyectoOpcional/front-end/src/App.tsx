c1f8ece6f153d43a0e6085e3cedfb300de26d9f9
Ary <aryelduranballestero@gmail.com>
2022-08-08 21:08:45 -0600

    Experimentación con react y flask

---
+++ ProyectoOpcional/front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Chessboard } from 'react-chessboard';
+
+function App() {
+  return (
+    <div>
+      <Chessboard/>
+    </div>
+  );
+}
+
+export default App;
