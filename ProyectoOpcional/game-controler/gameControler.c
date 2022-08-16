eaa97a0290ef2610e169014c91d137a4e107fc46
Isaac <irsacfac@gmail.com>
2022-08-15 22:25:37 -0600

    Update gameControler.c

--- ProyectoOpcional/game-controler/gameControler.c
+++ ProyectoOpcional/game-controler/gameControler.c
@@ -2,45 +2,16 @@
 #define gameControler
 
 #include "pieces.c"
+#include <stdbool.h>
 #include <math.h> //https://www.aprenderaprogramar.com/index.php?option=com_content&view=article&id=909:funciones-matematicas-en-c-redondeo-valor-absoluto-potencia-trigonometricas-raiz-cuadrada-cu00520f&catid=82&Itemid=210
 
 int board[8][8];
-piece chessPieces[32];
-
-bool movePiece(piece *p, int newRow, int newColumn){
-    if(p->column == newColumn && p->row == newRow){
-        return false;
-    }
-    enum pieceType pt = p->pieceType;
-    bool valid = false;
-    switch (pt)
-    {
-    case Bishop:
-        valid = validateBishopMove(p, newRow, newColumn);
-        break;
-    case Knight:
-        valid = validateKnightMove(p, newRow, newColumn);
-        break;
-    case Rook:
-        valid = validateRookMove(p, newRow, newColumn);
-        break;
-    case Queen:
-        valid = validateBishopMove(p, newRow, newColumn) || validateRookMove(p, newRow, newColumn);
-        break;
-    case King:
-        valid = validateKingMove(p, newRow, newColumn);
-        break;
-    default:
-        valid = validatePawnMove(p, newRow, newColumn);
-        break;
-    }
-    return true;
-}
+piece* chessPieces[32];
 
 bool validatePawnMove(piece *p, int newRow, int newColumn){
     if (p->white)
     {
-        if (newColumn != p->column)
+        if (newColumn != p->column && p->row +1 == newRow)
         {
             return false;
         }else if (4 == newRow && p->row == 2)
@@ -107,9 +78,46 @@ bool validateKingMove(piece *p, int newRow, int newColumn){
     return column<2&&row<2&&-2<column&&-2<row;
 }
 
+bool movePiece(piece *p, int newRow, int newColumn){
+    if(p->column == newColumn && p->row == newRow){
+        return false;
+    }
+    enum pieceType pt = p->pieceType;
+    bool valid = false;
+    switch (pt)
+    {
+    case Bishop:
+        valid = validateBishopMove(p, newRow, newColumn);
+        break;
+    case Knight:
+        valid = validateKnightMove(p, newRow, newColumn);
+        break;
+    case Rook:
+        valid = validateRookMove(p, newRow, newColumn);
+        break;
+    case Queen:
+        valid = validateBishopMove(p, newRow, newColumn) || validateRookMove(p, newRow, newColumn);
+        break;
+    case King:
+        valid = validateKingMove(p, newRow, newColumn);
+        break;
+    default:
+        valid = validatePawnMove(p, newRow, newColumn);
+        break;
+    }
+    int temp = board[newRow][newColumn];
+    if (temp != 0 && chessPieces[temp]->white == p->white){
+        valid = false;
+    }
+    return valid;
+}
+
 void upgradePiece();
 
-void newGame();
+void newGame(){
+    enum pieceType pt = Queen;
+    chessPieces[0] = newPieces(0, 5, 4, pt, true);
+}
 
 bool loadGame();
 
