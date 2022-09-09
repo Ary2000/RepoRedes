ed39492f435aa3d7c83005a90847866e8648e8f4
Ary <aryelduranballestero@gmail.com>
2022-09-09 00:28:58 -0600

    Window Reproductor.py

---
+++ Tarea1_Autrum/Documentacion.md
@@ -0,0 +1,16 @@
+Instituto Tecnológico de Costa Rica  
+Escuela de Computación  
+Redes GR 2  
+Tarea Corta 1: ¿Por qué las voces de los integrantes son diferentes?  
+Profesor Gerardo Nereo Campos Araya  
+Estudiantes
+
+- Ary-El Durán Ballestero | 2018102445
+- Isaac David Ortega Arguedas | 2018189196
+- Zhong Jie Liu Guo | 2018319114
+
+Fecha de Entrega 8/9/2022
+
+¿Por qué las voces de los integrantes son diferentes?
+
+Las voces de las personas son diferentes porque no todas llegan a las mismas notas, para esto esta el concepto de rango de voz, algunos pueden llegar a notas más graves mientras otros llegan a notas más agudas.
--- Tarea1_Autrum/Reproductor.py
+++ Tarea1_Autrum/Reproductor.py
@@ -8,18 +8,20 @@ from struct import pack
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from tkinter import TclError
 
-#%matplotlib tk
+# %matplotlib tk
 
-#https://www.geeksforgeeks.org/working-zip-files-python/
-#https://stackoverflow.com/questions/19371860/python-open-file-in-zip-without-temporarily-extracting-it
+# https://www.geeksforgeeks.org/working-zip-files-python/
+# https://stackoverflow.com/questions/19371860/python-open-file-in-zip-without-temporarily-extracting-it
 default_file_name = "output.atm"
 
 with ZipFile(default_file_name, 'r') as zip:
     for info in zip.infolist():
         print(info.filename)
         print('\tModified:\t' + str(datetime.datetime(*info.date_time)))
-        print('\tSystem:\t\t' + str(info.create_system) + '(0 = Windows, 3 = Unix)')
+        print('\tSystem:\t\t' + str(info.create_system) +
+              '(0 = Windows, 3 = Unix)')
         print('\tZIP version:\t' + str(info.create_version))
         print('\tCompressed:\t' + str(info.compress_size) + ' bytes')
         print('\tUncompressed:\t' + str(info.file_size) + ' bytes')
@@ -30,14 +32,14 @@ with ZipFile(default_file_name, 'r') as zip:
     file = zip.open("output.wav")
     jsonFile = zip.open("puntos.json")
 
-#https://www.youtube.com/watch?v=fVJTrcEqgIo
-#https://www.w3schools.com/python/python_json.asp
+# https://www.youtube.com/watch?v=fVJTrcEqgIo
+# https://www.w3schools.com/python/python_json.asp
 puntos = json.load(jsonFile)
 chunk = 1024 * 2
-sample_format = pyaudio.paInt16 
-channels = 1 # canal stereo
+sample_format = pyaudio.paInt16
+channels = 1  # canal stereo
 rate = 44100
-#print(puntos)
+# print(puntos)
 puntosAudio = puntos["puntosAudio"]
 puntosFFT = puntos["puntosFFT"]
 print(len(puntosAudio))
@@ -48,9 +50,13 @@ fig, (ax, ax2) = plt.subplots(2, figsize=(15, 8))
 
 x = np.arange(0, 2 * chunk, 2)
 xf = np.linspace(0, rate, chunk)     # frequencies (spectrum)
-line, = ax.plot(x, np.random.rand(chunk), '-')
+line, = ax.plot(x, np.random.rand(chunk), '-', lw=2)
+ax.set_title('AUDIO WAVEFORM')
+ax.set_xlabel('samples')
+ax.set_ylabel('volume')
 ax.set_ylim(0, 255)
 ax.set_xlim(0, chunk)
+plt.setp(ax, xticks=[0, chunk, 2 * chunk], yticks=[0, 128, 255])
 
 x_fft = np.linspace(0, rate, chunk)
 
@@ -58,25 +64,36 @@ line_fft, = ax2.semilogx(xf, np.random.rand(chunk), '-', lw=2)
 
 ax2.set_xlim(20, rate/2)
 
-#graph1 = FigureCanvasTkAgg(fig, ui)
-#graph1.get_tk_widget().grid(row=2, column=1)
-
-ax.plot()
 
 CHUNK = 1024
-wf = wave.open(file,'rb')
-p=pyaudio.PyAudio()
+wf = wave.open(file, 'rb')
+p = pyaudio.PyAudio()
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels = wf.getnchannels(),
+                channels=wf.getnchannels(),
                 rate=wf.getframerate(),
-                output = True)
+                output=True)
 data = wf.readframes(CHUNK)
+
+fig.show()
+
+frame_count = 0
 while len(data) > 0:
     stream.write(data)
     data = wf.readframes(CHUNK)
+
+    # update figure canvas
+    try:
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        frame_count += 1
+
+    except TclError:
+
+        break
 stream.stop_stream()
 stream.close()
 p.terminate()
+
 
 class Player:
     def __init__(self, file_name, CHUNK):
@@ -96,12 +113,12 @@ class Player:
         self.json = json.load(self.JSONfile)
 
     def play(self):
-        wf = wave.open(self.WAVfile,'rb')
-        p=pyaudio.PyAudio()
+        wf = wave.open(self.WAVfile, 'rb')
+        p = pyaudio.PyAudio()
         stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels = wf.getnchannels(),
+                        channels=wf.getnchannels(),
                         rate=wf.getframerate(),
-                        output = True)
+                        output=True)
         data = wf.readframes(self.CHUNK)
         while len(data) > 0:
             stream.write(data)
@@ -109,6 +126,7 @@ class Player:
         stream.stop_stream()
         stream.close()
         p.terminate()
+
 
 if __name__ == "__main__":
     chunk = 1024
