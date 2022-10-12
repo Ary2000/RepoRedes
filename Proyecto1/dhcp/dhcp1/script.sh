f42a5b7cb70d7cf84f1f9afbd112ae66a8584d3c
Ary <aryelduranballestero@gmail.com>
2022-10-11 22:41:53 -0600

    DNS cuantico  y con bitflop

--- Proyecto1/dhcp/dhcp1/script.sh
+++ Proyecto1/dhcp/dhcp1/script.sh
@@ -3,7 +3,4 @@ ip route del default
 ip route add default via 10.0.0.10
 truncate -s0 /etc/resolv.conf && bash -c 'echo "nameserver 10.0.0.3">> /etc/resolv.conf'
 
-truncate -s0 /etc/resolv.conf
-echo "nameserver 10.0.0.3" >> /etc/resolv.conf
-
 tail -f /dev/null
