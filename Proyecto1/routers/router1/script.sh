4190d6e015fd89a933dbd9ddbf755ecceb3fa6f6
unknown <liuguo35@hotmail.com>
2022-10-14 00:04:23 -0600

    Version no cuantica de dns

--- Proyecto1/routers/router1/script.sh
+++ Proyecto1/routers/router1/script.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 ip route add 10.0.1.0/24 via 10.0.0.2
+truncate -s0 /etc/resolv.conf
+bash -c 'echo "domain lan01.io" >> /etc/resolv.conf' && bash -c 'echo "nameserver 10.0.0.3" >> /etc/resolv.conf'
 
 #LOCAL
 sudo iptables-legacy -A INPUT -i lo -j ACCEPT
 #ENTRADA
+sudo iptables-legacy -A INPUT -p tcp --sport 80 -j ACCEPT
+sudo iptables-legacy -A INPUT -p tcp --sport 443-j ACCEPT
+sudo iptables-legacy -A INPUT -p tcp --sport 3128 -j ACCEPT
+sudo iptables-legacy -A INPUT -p tcp --sport 8443 -j ACCEPT
+sudo iptables-legacy -A INPUT -p udp --sport 53 -j ACCEPT
+sudo iptables-legacy -A INPUT -p tcp --sport 53 -j ACCEPT
+
 sudo iptables-legacy -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables-legacy -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables-legacy -A INPUT -p tcp --dport 3128 -j ACCEPT
 sudo iptables-legacy -A INPUT -p tcp --dport 8443 -j ACCEPT
 sudo iptables-legacy -A INPUT -p udp --dport 53 -j ACCEPT
 sudo iptables-legacy -A INPUT -p tcp --dport 53 -j ACCEPT
+
 sudo iptables-legacy -A INPUT -p icmp -j ACCEPT
 #NEGAR TODO LO DEMAS
 sudo iptables-legacy -A INPUT -j DROP
@@ -18,30 +28,33 @@ sudo iptables-legacy -A INPUT -j DROP
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sudo iptables-legacy -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
+sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+
 #PROXY REVERSO
-sudo iptables-legacy -A FORWARD -p tcp -d 10.0.0.20 --dport 80 -j ACCEPT
-sudo iptables-legacy -A FORWARD -p tcp -d 10.0.0.20 --dport 443 -j ACCEPT
+sudo iptables-legacy -A FORWARD -p tcp -s 10.0.0.20 --dport 80 -j ACCEPT
+sudo iptables-legacy -A FORWARD -p tcp -s 10.0.0.20 --dport 443 -j ACCEPT
 
 #WEB CACHE
-sudo iptables-legacy -A FORWARD -p tcp -d 10.0.0.7 --dport 3128 -j ACCEPT
+sudo iptables-legacy -A FORWARD -p tcp -s 10.0.0.7 --dport 3128 -j ACCEPT
 
 #VPN
 sudo iptables-legacy -A FORWARD -p tcp -d 10.0.1.10 --dport 8443 -j ACCEPT 
 
 #DNS
-sudo iptables-legacy -A FORWARD -p udp -d 10.0.0.3 --dport 53 -j ACCEPT
-sudo iptables-legacy -A FORWARD -p tcp -d 10.0.0.3 --dport 53 -j ACCEPT
+sudo iptables-legacy -A FORWARD -p udp -s 10.0.0.3 --dport 53 -j ACCEPT
+sudo iptables-legacy -A FORWARD -p tcp -s 10.0.0.3 --dport 53 -j ACCEPT
 
 sudo iptables-legacy -A FORWARD -p tcp -s 10.0.1.0/24 -j ACCEPT
 sudo iptables-legacy -A FORWARD -p udp -s 10.0.1.0/24 -j ACCEPT
 
-truncate -s0 /etc/resolv.conf
-echo "nameserver 10.0.0.3" >> /etc/resolv.conf
-
 #SALIDA
-sudo iptables-legacy -A OUTPUT -p udp -j ACCEPT
-sudo iptables-legacy -A OUTPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables-legacy -A OUTPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -o lo -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p udp --sport 53 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p tcp --sport 53 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p tcp --sport 80 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p tcp --sport 443 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p tcp --sport 3128 -j ACCEPT
+sudo iptables-legacy -A OUTPUT -p tcp --sport 8443 -j ACCEPT
 
 # CMD para mantenerlo levantado
 tail -f /dev/null
--- Proyecto1/routers/router2/script.sh
+++ Proyecto1/routers/router2/script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo ip route del default 
 sudo ip route add default via 10.0.0.10
+truncate -s0 /etc/resolv.conf
+bash -c 'echo "domain lan02.io" >> /etc/resolv.conf' && bash -c 'echo "nameserver 10.0.0.3" >> /etc/resolv.conf'
 
 #LOCAL
 sudo iptables-legacy -A INPUT -i lo -j ACCEPT
@@ -9,11 +11,18 @@ sudo iptables-legacy -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables-legacy -A INPUT -p udp --dport 80 -j ACCEPT
 sudo iptables-legacy -A INPUT -p icmp -j ACCEPT
 
+iptables-legacy -A INPUT -i eth0 -m state --state NEW -p tcp --dport 8443 -j ACCEPT
+iptables-legacy -A INPUT -i tun+ -j ACCEPT
+
+
 #MASQUERADE https://bobcares.com/blog/iptables-nat-masquerade/ 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sudo iptables-legacy -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 #FORWARD
+iptables-legacy -A FORWARD -i tun+ -j ACCEPT
+iptables-legacy -A FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables-legacy -A FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables-legacy -A FORWARD -p tcp -s 10.0.0.0/24 --dport 80 -j ACCEPT
 sudo iptables-legacy -A FORWARD -p udp -s 10.0.0.0/24 --dport 80 -j ACCEPT
 sudo iptables-legacy -A FORWARD -p tcp -s 10.0.1.0/24 -d 10.0.0.0/24 --dport 80 -j ACCEPT
@@ -25,6 +34,7 @@ truncate -s0 /etc/resolv.conf
 echo "nameserver 10.0.0.3" >> /etc/resolv.conf
 
 #SALIDA
+iptables-legacy -A OUTPUT -o tun+ -j ACCEPT
 sudo iptables-legacy -A OUTPUT -p tcp -j ACCEPT
 sudo iptables-legacy -A OUTPUT -p udp -j ACCEPT
 
