3e98cc6bc46a421b9c8f0eb3c2b4ddd815ac4c7e
Ary <aryelduranballestero@gmail.com>
2022-10-14 14:11:37 -0600

    Squid cache

--- Proyecto1/cache/script.sh
+++ Proyecto1/cache/script.sh
@@ -1,3 +1,6 @@
+sudo truncate -s0 /etc/resolv.conf
+sudo bash -c 'echo "domain lan01.io" >> /etc/resolv.conf' && bash -c 'echo "nameserver 10.0.0.3" >> /etc/resolv.conf'
+
 #sudo iptables-legacy -A PREROUTING -s 10.0.0.0/24 -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128
 
 sudo iptables-legacy -t nat -A PREROUTING -s 10.0.0.0/24 -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128
