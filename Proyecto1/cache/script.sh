49aa45137390d5503d96d3476cbab80da1afee7f
Ary <aryelduranballestero@gmail.com>
2022-10-21 17:18:40 -0600

    Cache con internet

--- Proyecto1/cache/script.sh
+++ Proyecto1/cache/script.sh
@@ -1,9 +1,9 @@
+ip route del default 
+ip route add default via 10.0.0.10
 sudo truncate -s0 /etc/resolv.conf
 sudo bash -c 'echo "domain lan01.io" >> /etc/resolv.conf' && bash -c 'echo "nameserver 10.0.0.3" >> /etc/resolv.conf'
 
 #sudo iptables-legacy -A PREROUTING -s 10.0.0.0/24 -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128
-
-sudo iptables-legacy -t nat -A PREROUTING -s 10.0.0.0/24 -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3128
 
 # CMD para mantenerlo levantado
 tail -f /dev/null
