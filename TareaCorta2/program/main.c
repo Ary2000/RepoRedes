1941c767f35b5f4968e8f1c4791d384ca55a1b2f
MarioFR10 <mariosmite123@gmail.com>
2022-10-25 10:09:10 -0600

    random subnets

--- TareaCorta2/program/main.c
+++ TareaCorta2/program/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -193,7 +192,21 @@ char* get_hosts_range(char* p_ip, char* p_mask) {
     strcat(respuesta, "-");
     strcat(respuesta, maximoString);
     return respuesta;
-    //printf("Rango de Host IPs de %s %s es: %s\n", p_ip, p_mask, maximoString);
+}
+
+char** get_random_subnet(char* p_ip, char* p_mask, int n, char** result) {
+
+    int min = get_network_number(p_ip, p_mask) + 1;
+    int max = get_broadcast(p_ip, p_mask) - 1;
+    int total_hosts = max - min + 1;
+
+    for (int i = 1; i <= n; i++) {
+        int r = rand() % total_hosts;
+        char* rand_ip = int_to_ip(min + r);
+        result[i-1] = rand_ip;
+        printf("Randon subnet #%d -> %s\n", i, rand_ip);
+    }
+    return result;
 }
 
 void comunication(int connfd) {
@@ -265,53 +278,62 @@ int main(){
   
     char* ip = "172.16.0.56";
     char* mask = "/25";
-    char* mask1 = "255.255.255.128";
-     
-    int sockfd, connfd, len;
-    struct sockaddr_in servaddr, cli;
-   
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-   
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
-   
-    // Binding newly created socket to given IP and verification
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully binded..\n");
-   
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
-        printf("Listen failed...\n");
-        exit(0);
-    }
-    else
-        printf("Server listening..\n");
-    len = sizeof(cli);
-   
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0) {
-        printf("server accept failed...\n");
-        exit(0);
-    }
-    else
-        printf("server accept the client...\n");
+    char* mask1 = "255.255.255.248";
+    int n = 3;
+    
+    char* result[n];
 
-    comunication(connfd);
+    get_random_subnet(ip, mask, n, result);
+
+    for (int i = 0; i < n; i++){
+        printf("%d -> %s\n", i, result[i]);
+    }
+     
+    // int sockfd, connfd, len;
+    // struct sockaddr_in servaddr, cli;
+   
+    // // socket create and verification
+    // sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // if (sockfd == -1) {
+    //     printf("socket creation failed...\n");
+    //     exit(0);
+    // }
+    // else
+    //     printf("Socket successfully created..\n");
+    // bzero(&servaddr, sizeof(servaddr));
+   
+    // // assign IP, PORT
+    // servaddr.sin_family = AF_INET;
+    // servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // servaddr.sin_port = htons(PORT);
+   
+    // // Binding newly created socket to given IP and verification
+    // if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+    //     printf("socket bind failed...\n");
+    //     exit(0);
+    // }
+    // else
+    //     printf("Socket successfully binded..\n");
+   
+    // // Now server is ready to listen and verification
+    // if ((listen(sockfd, 5)) != 0) {
+    //     printf("Listen failed...\n");
+    //     exit(0);
+    // }
+    // else
+    //     printf("Server listening..\n");
+    // len = sizeof(cli);
+   
+    // // Accept the data packet from client and verification
+    // connfd = accept(sockfd, (SA*)&cli, &len);
+    // if (connfd < 0) {
+    //     printf("server accept failed...\n");
+    //     exit(0);
+    // }
+    // else
+    //     printf("server accept the client...\n");
+
+    // comunication(connfd);
   
   // Get Broadcast IP zone
     /*
@@ -340,7 +362,6 @@ int main(){
     */
 	
     // End of Get Network Number IP zone
-    //get_hosts_range(ip, mask);
-
+    
 	return 0;
 }
